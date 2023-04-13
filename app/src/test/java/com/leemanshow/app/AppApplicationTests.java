5fb2f542d37f606814a6d2fd0711457472a5f4e1
liwencan <13802885134@139.com>
2023-04-13 08:56:06 +0800

    单体形式的app

---
+++ app/src/test/java/com/leemanshow/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leemanshow.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
