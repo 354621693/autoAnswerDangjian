f55e53b94c6183bb84c7028cdbd71520526b28ce
Liwencan <354621693@qq.com>
2023-04-14 17:24:59 +0800

    暂存

--- app/src/main/java/com/leemanshow/app/es/service/ElasticsearchService.java
+++ app/src/main/java/com/leemanshow/app/es/service/ElasticsearchService.java
@@ -1,4 +1,6 @@
 package com.leemanshow.app.es.service;
 
 public interface ElasticsearchService {
+
+    public String find(String query);
 }
--- app/src/main/java/com/leemanshow/app/es/service/ElasticsearchServiceImpl.java
+++ app/src/main/java/com/leemanshow/app/es/service/ElasticsearchServiceImpl.java
@@ -1,9 +1,12 @@
 package com.leemanshow.app.es.service;
 
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @Service
 public class ElasticsearchServiceImpl implements ElasticsearchService {
@@ -12,5 +15,12 @@ public class ElasticsearchServiceImpl implements ElasticsearchService {
 
     public String find(String query){
         return null;
+    }
+
+    void testCreateIndex() throws IOException {
+
+        CreateIndexRequest request = new CreateIndexRequest("books");
+        client.indices().create(request, RequestOptions.DEFAULT);
+
     }
 }
